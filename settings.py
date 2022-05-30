9bfd1390b4cb75915098733ade723078a2203c3d
SteveAmos <somaevets@gmail.com>
2022-05-30 11:58:45 -0500

    Fix errors in settings.py

--- settings.py
+++ settings.py
@@ -1,37 +1,6 @@
-import sys
-import pygame
-
-from settings import Settings
-from ship import Ship
-
-class AlienInvasion:
-    """Overall class to manage game assets and behavior"""
-
+class Settings:
     def __init__(self):
-        """Initialize game and create game resources"""
-        pygame.init()
-        self.settings = Settings()
-
-        self.screen = pygame.display.set_mode((
-            self.settings.screen_width,self.settings.screen_height))
-        pygame.display.set_caption("Alien Invasion")
-        
-        self.ship = Ship(self)
-
-    def run_game(self):
-        """Start main loop for game"""
-        while True:
-            #Watch for key and mouse events
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-            #Redraw screen during each loop pass
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-
-            #Make most recently drawn screen visible
-            pygame.display.flip()
-
-if __name__ == '__main__':
-    ai = AlienInvasion()
-    ai.run_game()
+        #Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (100, 100, 180)
